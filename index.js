0a5396bc4c5f399deecccd6c66b3f18727b50f3e
Carmen L. Sandoval <lucero@Carmens-Air.attlocal.net>
2021-08-25 13:41:53 -0500

    jQuery animations

---
+++ index.js
@@ -0,0 +1,10 @@
+$(".landingDiv").mouseover(function(){
+    $(".landingDiv h1").slideDown("slow");
+});
+ 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    $(".portrait").css({
+      transform: 'translate3d(0, +'+(scroll/100)+'%, 0) scale('+(100 - scroll/40)/75+')'  
+    });
+  });
